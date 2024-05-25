6fbcd806d954b3ab6f4ab822dfa76b372e7509aa
andresvpn <125711121+andresvpn@users.noreply.github.com>
2024-05-25 11:20:49 -0500

    Create user.js

---
+++ user.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    { id: 1, nombre: "1234", contraseña: "1234", fechaVencimiento: "2024-06-01" },
+    { id: 2, nombre: "andresvpn", contraseña: "1088829889", fechaVencimiento: "2024-06-24" },
+    // Agrega más usuarios si es necesario
+];
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    const usuario = usuarios.find(user => user.nombre === username && user.contraseña === password);
+    if (usuario) {
+        const fechaVencimiento = new Date(usuario.fechaVencimiento);
+        const fechaActual = new Date();
+
+        if (fechaActual > fechaVencimiento) {
+            errorMessage.textContent = 'Usuario y contraseña han vencido';
+        } else {
+            errorMessage.textContent = ''; // Clear any previous error messages
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 1000);
+        }
+    } else {
+        errorMessage.textContent = 'Usuario o contraseña incorrectos';
+    }
+});
